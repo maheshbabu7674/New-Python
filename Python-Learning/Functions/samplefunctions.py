54b5a8ee921dfc281d91037cd845c119e6484daa
Mahesh Babu Miriyampalli <jesuscyrilpaul@gmail.com>
2024-02-19 13:25:55 +0530

    added

---
+++ Python-Learning/Functions/samplefunctions.py
@@ -0,0 +1,40 @@
+# Example 1:
+ #Declaring the function:
+
+def myfun():
+    print("hello")
+
+# Calling the function:
+    
+myfun()    #hello
+
+# Example 2:
+
+def myfun(name):
+    print("hello",name)
+
+myfun("mahesh")        #hello mahesh
+
+# Example 3:
+
+def cal(a,b):
+    return(a+b)
+
+sum=cal(100,200)
+print(sum)
+
+##   or    ##
+
+print(cal(10,20))
+
+
+# Example 4:
+
+def func():
+    return
+
+func()
+
+##  or  ##
+
+print(func())  ## None

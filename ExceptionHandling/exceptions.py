40e2f91da47c5c32cb0d7d19cb6a99bf797efaba
Mahesh <jesuscyrilpaul@gmail.com>
2024-02-22 04:34:04 +0530

    added exceptions

---
+++ ExceptionHandling/exceptions.py
@@ -0,0 +1,14 @@
+# Example 1:
+
+print("Initializing the program")
+print("Started the program")
+print("Program is in progress")
+
+try:
+    print(x)
+except:
+    print("Exception occured")
+
+print("Program is running")
+print("Program ended")
+
